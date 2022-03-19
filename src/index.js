42ddf20d17bc3699e2e7ff9ef9e96d2b0afe4c97
Syed Maqutadir Rehman <maqutadir.rehman@gmail.com>
2022-03-19 10:57:26 -0400

    Cleared contents of index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const now = Date.now();
-
-const log = (string, func) => console.log(string, func);
-
-log("now", now);
-console.log("hello world");
-console.log("Lets go there");
-alert("here I am");
-console.log("okay");
